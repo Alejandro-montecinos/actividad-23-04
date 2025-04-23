d3d3e3e099a75532c6bd8447bf146b7da412826c
Alejandro-montecinos <alejandromontecino002@gmail.com>
2025-04-23 10:31:12 -0400

    se añade la actividad

---
+++ actividad-23-04/src/main/java/com/example/actividad_23_04/Actividad2304Application.java
@@ -0,0 +1,13 @@
+package com.example.actividad_23_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad2304Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad2304Application.class, args);
+	}
+
+}
---
+++ actividad-23-04/src/test/java/com/example/actividad_23_04/Actividad2304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actividad_23_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad2304ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
