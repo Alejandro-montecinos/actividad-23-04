5d70070be84ae517e5e6f944db9d1f159044c4fe
alejandro montecinos <alejandromontecino002@gmail.com>
2025-04-23 11:23:34 -0400

    se modifica el metodo para variable nombre

--- actividad-23-04/src/main/java/com/example/actividad_23_04/controller/helloworld.java
+++ actividad-23-04/src/main/java/com/example/actividad_23_04/controller/helloworld.java
@@ -1,15 +1,18 @@
 package com.example.actividad_23_04.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/API")
 public class helloworld {
 
     @GetMapping("/hola")
-    public String HolaMundo(){
-        return "HOLAAAAAA";
+
+    public String HolaMundo(@RequestParam String nombre){
+        return "mi nombre es"+nombre;
 
     }
 
