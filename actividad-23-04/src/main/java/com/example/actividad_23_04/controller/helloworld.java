e6d560ca5efaf37687b7527feebf682100d05f5c
alejandro montecinos <alejandromontecino002@gmail.com>
2025-04-24 14:59:39 -0400

    Se agregan las operaciones aritmeticas suma/ resta/ multiplicacion/ division

--- actividad-23-04/src/main/java/com/example/actividad_23_04/controller/helloworld.java
+++ actividad-23-04/src/main/java/com/example/actividad_23_04/controller/helloworld.java
@@ -6,14 +6,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/API")
+@RequestMapping("/api")
 public class helloworld {
 
-    @GetMapping("/hola")
-
-    public String HolaMundo(@RequestParam String nombre){
-        return "mi nombre es"+nombre;
-
+    @GetMapping("/suma")
+    public String suma(int num1, int num2){
+        return "/api/suma " + String.valueOf(num1+num2);
     }
+    @GetMapping("/resta")
+    public String resta(int num1, int num2){
+        return "/api/resta " + String.valueOf(num1-num2);
+    }
+    @GetMapping("/multiplicacion")
+    public String multi(int num1, int num2){
+        return "/api/multiplicacion " + String.valueOf(num1*num2);
+    }
+    @GetMapping("/division")
+    public String divi(int num1, int num2){
+        return "/api/division " + String.valueOf( (double) num1/num2);
+    }
+
+
 
 }
