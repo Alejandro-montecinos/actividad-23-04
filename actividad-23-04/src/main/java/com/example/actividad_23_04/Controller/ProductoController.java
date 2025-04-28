e2c55e1d297a30a6383f901609242111cd450d3c
alejandro montecinos <alejandromontecino002@gmail.com>
2025-04-28 10:33:41 -0400

    se añade un metodo lista

--- actividad-23-04/src/main/java/com/example/actividad_23_04/Controller/ProductoController.java
+++ actividad-23-04/src/main/java/com/example/actividad_23_04/Controller/ProductoController.java
@@ -1,8 +1,13 @@
 package com.example.actividad_23_04.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/producto")
@@ -12,4 +17,24 @@ public class ProductoController {
     public String index(){
         return"bienvenido a productos!!!!!";
     }
+    @GetMapping("/frutas")
+    public String buscador(int inde){
+        try {
+
+
+            List<String> listas = new ArrayList<>();
+            listas.add("manzana");
+            listas.add("perap");
+            listas.add("pipzazz");
+            listas.add("holas");
+            listas.add("el papu 1234");
+
+
+            return listas.get(inde);
+        } catch (Exception e) {
+            return String.valueOf(ResponseEntity.status(HttpStatus.NOT_FOUND).body("Recurso no encontrado"));
+        }
+
+    }
+
 }
